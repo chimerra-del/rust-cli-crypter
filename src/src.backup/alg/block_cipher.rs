51e7b29f7eb3e576d50724f0fcdbabd74c11a101
chimerra-del <232224040+chimerra-del@users.noreply.github.com>
2026-09-03 20:58:49 +0600

    Немного более стабильная версия

--- src/src.backup/alg/block_cipher.rs
+++
@@ -1,6 +0,0 @@
-pub trait BlockCipher: Send + Sync {
-    fn block_size(&self) -> usize;
-    fn encrypt_block(&self, plaintext: &[u8]) -> Vec<u8>;
-    fn decrypt_block(&self, ciphertext: &[u8]) -> Vec<u8>;
-    fn name(&self) -> &str;
-}
--- src/src.backup/passwd_hashing/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod hkdf;
-
-pub use hkdf::hkdf_expand;
-pub use hkdf::hkdf_extract;
